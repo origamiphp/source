0f554f6c00cedd905a35f7e8a519802d73c33fa6
Alexandre Jardin <info@ajardin.fr>
2019-05-30 21:30:20 +0200

    Fix too restrictive dependencies

--- src/Manager/ApplicationLock.php
+++ src/Manager/ApplicationLock.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace App\Manager;
 
 use App\Exception\ApplicationLockException;
-use Symfony\Component\Cache\Simple\FilesystemCache;
+use Psr\Cache\InvalidArgumentException;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Component\Cache\CacheItem;
 
 class ApplicationLock
 {
@@ -15,16 +17,18 @@ class ApplicationLock
      * Generates the lock entry used to avoid duplicate launches.
      *
      * @param string $project
-     *
      * @throws ApplicationLockException
      */
     public function generateLock(string $project): void
     {
-        try {
-            $cache = new FilesystemCache();
-            $cache->set(self::LOCK_CACHE_KEY, $project);
-        } catch (\Psr\SimpleCache\InvalidArgumentException $e) {
-            throw new ApplicationLockException($e->getMessage());
+        $cache = new FilesystemAdapter();
+
+        /** @var CacheItem $cacheItem */
+        $cacheItem = $cache->getItem(self::LOCK_CACHE_KEY);
+        $cacheItem->set($project);
+
+        if (!$cache->save($cacheItem)) {
+            throw new ApplicationLockException('Unable to generate the application lock.');
         }
     }
 
@@ -35,24 +39,24 @@ class ApplicationLock
      */
     public function getCurrentLock(): string
     {
-        $cache = new FilesystemCache();
+        $cache = new FilesystemAdapter();
+        $cacheItem = $cache->getItem(self::LOCK_CACHE_KEY);
 
-        try {
-            $project = $cache->get(self::LOCK_CACHE_KEY, false);
-            $result = !empty($project) ? $project : '';
-        } catch (\Psr\SimpleCache\InvalidArgumentException $e) {
-            $result = '';
-        }
-
-        return $result;
+        return $cacheItem->isHit() ? $cacheItem->get() : '';
     }
 
     /**
      * Removes the lock entry used to avoid duplicate launches.
+     *
+     * @throws ApplicationLockException
      */
     public function removeLock(): void
     {
-        $cache = new FilesystemCache();
-        $cache->delete(self::LOCK_CACHE_KEY);
+        try {
+            $cache = new FilesystemAdapter();
+            $cache->delete(self::LOCK_CACHE_KEY);
+        } catch (InvalidArgumentException $e) {
+            throw new ApplicationLockException($e->getMessage());
+        }
     }
 }
