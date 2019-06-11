4a180bdee18a256941908402c6d3e4fcc1c74c7b
Alexandre Jardin <info@ajardin.fr>
2019-06-12 00:00:41 +0200

    Prepare the bundling with Box

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
