2c379fa2994f0a1a3a532d1c3a02d6909725de0b
Alexandre Jardin <info@ajardin.fr>
2020-01-24 00:21:39 +0100

    Improve the console display

---
+++ src/Application.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use App\Command\DefaultCommand;
+use Symfony\Bundle\FrameworkBundle\Console\Application as SymfonyApplication;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class Application extends SymfonyApplication
+{
+    public const CONSOLE_LOGO = <<<'ASCII'
+  ___       _                       _
+ / _ \ _ __(_) __ _  __ _ _ __ ___ (_)
+| | | | '__| |/ _` |/ _` | '_ ` _ \| |
+| |_| | |  | | (_| | (_| | | | | | | |
+ \___/|_|  |_|\__, |\__,_|_| |_| |_|_|
+              |___/
+
+
+ASCII;
+
+    public const CONSOLE_NAME = 'Origami';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct(KernelInterface $kernel)
+    {
+        parent::__construct($kernel);
+
+        if ($defaultName = DefaultCommand::getDefaultName()) {
+            $this->setDefaultCommand($defaultName);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getHelp(): string
+    {
+        return self::CONSOLE_LOGO.parent::getHelp();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName(): string
+    {
+        return self::CONSOLE_NAME;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getVersion(): string
+    {
+        return preg_match('/^v\d+\.\d+\.\d+/', '@app_version@')
+            ? ltrim('@app_version@', 'v') : '@app_version@';
+    }
+}
---
+++ tests/ApplicationTest.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Application;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * @internal
+ *
+ * @covers \App\Application
+ *
+ * @uses \App\Command\Additional\CheckCommand
+ * @uses \App\Command\Additional\RegisterCommand
+ * @uses \App\Command\Additional\RegistryCommand
+ * @uses \App\Command\Contextual\DataCommand
+ * @uses \App\Command\Contextual\LogsCommand
+ * @uses \App\Command\Contextual\PsCommand
+ * @uses \App\Command\Contextual\RestartCommand
+ * @uses \App\Command\Contextual\RootCommand
+ * @uses \App\Command\Contextual\Services\AbstractServiceCommand
+ * @uses \App\Command\Contextual\Services\ElasticsearchCommand
+ * @uses \App\Command\Contextual\Services\MysqlCommand
+ * @uses \App\Command\Contextual\Services\NginxCommand
+ * @uses \App\Command\Contextual\Services\PhpCommand
+ * @uses \App\Command\Contextual\Services\RedisCommand
+ * @uses \App\Command\Contextual\StopCommand
+ * @uses \App\Command\Main\InstallCommand
+ * @uses \App\Command\Main\PrepareCommand
+ * @uses \App\Command\Main\StartCommand
+ * @uses \App\Command\Main\UninstallCommand
+ * @uses \App\Command\AbstractBaseCommand
+ * @uses \App\Command\DefaultCommand
+ * @uses \App\Helper\ProcessFactory
+ * @uses \App\Kernel
+ * @uses \App\Middleware\Binary\DockerCompose
+ * @uses \App\Middleware\Binary\Mkcert
+ * @uses \App\Middleware\SystemManager
+ * @uses \App\Repository\EnvironmentRepository
+ */
+final class ApplicationTest extends WebTestCase
+{
+    public function testItPrintTheApplicationHeader(): void
+    {
+        $kernel = self::bootKernel();
+        $application = new Application($kernel);
+
+        $commandTester = new CommandTester($application->get('list'));
+        $commandTester->execute([]);
+
+        static::assertStringContainsString(Application::CONSOLE_LOGO, $commandTester->getDisplay());
+        static::assertStringContainsString(Application::CONSOLE_NAME.' @app_version@', $commandTester->getDisplay());
+    }
+
+    public function testItDoesNotPrintTheApplicationHeader(): void
+    {
+        $kernel = self::bootKernel();
+        $application = new Application($kernel);
+
+        $commandTester = new CommandTester($application->get('registry'));
+        $commandTester->execute([]);
+
+        static::assertStringNotContainsString(Application::CONSOLE_LOGO, $commandTester->getDisplay());
+        static::assertStringNotContainsString(Application::CONSOLE_NAME.' @app_version@', $commandTester->getDisplay());
+    }
+}
