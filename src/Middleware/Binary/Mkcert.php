018a67ab3bf082320be9354e82c2ed264fb3236f
Alexandre Jardin <info@ajardin.fr>
2021-01-05 16:27:11 +0100

    Improve the mkcert configuration

--- src/Middleware/Binary/Mkcert.php
+++ src/Middleware/Binary/Mkcert.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace App\Middleware\Binary;
 
+use App\Exception\MkcertException;
 use App\Helper\ProcessFactory;
 
 class Mkcert
 {
+    private const DEFAULT_DOMAINS = ['localhost', '127.0.0.1', '::1'];
+
     /** @var ProcessFactory */
     private $processFactory;
 
@@ -18,12 +21,37 @@ class Mkcert
 
     /**
      * Generates a locally-trusted development certificate with mkcert.
+     *
+     * @throws MkcertException
      */
     public function generateCertificate(string $certificate, string $privateKey, array $domains): bool
     {
-        $command = array_merge(['mkcert', '-cert-file', $certificate, '-key-file', $privateKey], $domains);
+        $this->installCertificateAuthority();
+
+        $command = array_merge(
+            ['mkcert', '-cert-file', $certificate, '-key-file', $privateKey],
+            self::DEFAULT_DOMAINS,
+            $domains
+        );
         $process = $this->processFactory->runBackgroundProcess($command);
 
         return $process->isSuccessful();
+    }
+
+    /**
+     * Installs the local certificate authority in the system trust store.
+     *
+     * @throws MkcertException
+     */
+    private function installCertificateAuthority(): void
+    {
+        $command = ['mkcert', '-install'];
+        $process = $this->processFactory->runBackgroundProcess($command);
+
+        if (!$process->isSuccessful()) {
+            throw new MkcertException(
+                sprintf("Unable to install the local certificate authority.\n%s", $process->getOutput())
+            );
+        }
     }
 }
--- tests/Middleware/Binary/MkcertTest.php
+++ tests/Middleware/Binary/MkcertTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Tests\Middleware\Binary;
 
+use App\Exception\MkcertException;
 use App\Helper\ProcessFactory;
 use App\Middleware\Binary\Mkcert;
 use App\Tests\CustomProphecyTrait;
@@ -27,11 +28,40 @@ final class MkcertTest extends TestCase
     {
         [$processFactory] = $this->prophesizeObjectArguments();
 
-        $process = $this->prophesize(Process::class);
-        $command = array_merge(['mkcert', '-cert-file', './custom.pem', '-key-file', './custom.key'], $domains);
+        $installProcess = $this->prophesize(Process::class);
+        $installCommand = ['mkcert', '-install'];
 
-        $process->isSuccessful()->shouldBeCalledOnce()->willReturn(true);
-        $processFactory->runBackgroundProcess($command)->shouldBeCalledOnce()->willReturn($process->reveal());
+        $processFactory->runBackgroundProcess($installCommand)->shouldBeCalledOnce()->willReturn($installProcess->reveal());
+        $installProcess->isSuccessful()->shouldBeCalledOnce()->willReturn(true);
+
+        $generateProcess = $this->prophesize(Process::class);
+        $generateCommand = array_merge(
+            ['mkcert', '-cert-file', './custom.pem', '-key-file', './custom.key', 'localhost', '127.0.0.1', '::1'],
+            $domains
+        );
+
+        $processFactory->runBackgroundProcess($generateCommand)->shouldBeCalledOnce()->willReturn($generateProcess->reveal());
+        $generateProcess->isSuccessful()->shouldBeCalledOnce()->willReturn(true);
+
+        $mkcert = new Mkcert($processFactory->reveal());
+        static::assertTrue($mkcert->generateCertificate('./custom.pem', './custom.key', $domains));
+    }
+
+    /**
+     * @dataProvider provideCertificateDomains
+     */
+    public function testItThrowsAnExceptionWhenInstallFails(array $domains): void
+    {
+        [$processFactory] = $this->prophesizeObjectArguments();
+
+        $installProcess = $this->prophesize(Process::class);
+        $installCommand = ['mkcert', '-install'];
+
+        $processFactory->runBackgroundProcess($installCommand)->shouldBeCalledOnce()->willReturn($installProcess->reveal());
+        $installProcess->isSuccessful()->shouldBeCalledOnce()->willReturn(false);
+        $installProcess->getOutput()->shouldBeCalledOnce();
+
+        $this->expectException(MkcertException::class);
 
         $mkcert = new Mkcert($processFactory->reveal());
         static::assertTrue($mkcert->generateCertificate('./custom.pem', './custom.key', $domains));
@@ -39,9 +69,8 @@ final class MkcertTest extends TestCase
 
     public function provideCertificateDomains(): Generator
     {
-        yield 'Magento domain' => [['magento2.localhost']];
-        yield 'Sylius domain' => [['sylius.localhost']];
-        yield 'Symfony domain' => [['symfony.localhost']];
+        yield 'Single domain' => [['symfony.localhost']];
+        yield 'Multiple domains' => [['symfony.localhost', '*.symfony.localhost']];
     }
 
     /**
