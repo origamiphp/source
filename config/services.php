fb51dfa928320d6bb39663fe5c92dac251b106e8
Alexandre Jardin <info@ajardin.fr>
2021-12-27 17:28:53 +0100

    Refresh the technical requirements for each supported technology

--- config/services.php
+++ config/services.php
@@ -36,48 +36,72 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters->set('app.requirements', [
         // https://www.drupal.org/docs/system-requirements
         EnvironmentEntity::TYPE_DRUPAL => [
+            '9.3' => [
+                'database' => ['mariadb:10.5', 'mariadb:10.4', 'mariadb:10.3', 'mysql:8.0', 'mysql:5.7', 'postgres:10-alpine'],
+                'php' => ['ajardin/php:8.1', 'ajardin/php:8.0', 'ajardin/php:7.4', 'ajardin/php:7.3'],
+            ],
+            '9.2' => [
+                'database' => ['mariadb:10.5', 'mariadb:10.4', 'mariadb:10.3', 'mysql:8.0', 'mysql:5.7', 'postgres:10-alpine'],
+                'php' => ['ajardin/php:8.0', 'ajardin/php:7.4', 'ajardin/php:7.3'],
+            ],
             '9.1' => [
                 'database' => ['mariadb:10.5', 'mariadb:10.4', 'mariadb:10.3', 'mysql:8.0', 'mysql:5.7', 'postgres:10-alpine'],
                 'php' => ['ajardin/php:8.0', 'ajardin/php:7.4', 'ajardin/php:7.3'],
             ],
             '9.0' => [
                 'database' => ['mariadb:10.5', 'mariadb:10.4', 'mariadb:10.3', 'mysql:8.0', 'mysql:5.7', 'postgres:10-alpine'],
-                'php' => ['ajardin/php:8.0', 'ajardin/php:7.4', 'ajardin/php:7.3'],
-            ],
-            '8.9' => [
-                'database' => ['mariadb:10.5', 'mariadb:10.4', 'mariadb:10.3', 'mariadb:10.2', 'mariadb:10.1', 'mysql:8.0', 'mysql:5.7', 'mysql:5.6', 'postgres:9-alpine'],
-                'php' => ['ajardin/php:7.4', 'ajardin/php:7.3'],
-            ],
-            '8.8' => [
-                'database' => ['mariadb:10.5', 'mariadb:10.4', 'mariadb:10.3', 'mariadb:10.2', 'mariadb:10.1', 'mysql:8.0', 'mysql:5.7', 'mysql:5.6', 'postgres:9-alpine'],
                 'php' => ['ajardin/php:7.4', 'ajardin/php:7.3'],
             ],
         ],
 
         // https://devdocs.magento.com/guides/v2.4/install-gde/system-requirements.html
         EnvironmentEntity::TYPE_MAGENTO2 => [
-            '2.4' => [
-                'database' => ['mariadb:10.4', 'mariadb:10.3', 'mariadb:10.2', 'mysql:8.0', 'mysql:5.7'],
-                'elasticsearch' => ['ajardin/magento2-elasticsearch:7', 'ajardin/magento2-elasticsearch:6'],
-                'php' => ['ajardin/php:7.4', 'ajardin/php:7.3'],
-                'redis' => ['redis:6-alpine', 'redis:5-alpine'],
+            '2.4.5' => [
+                'database' => ['mariadb:10.4', 'mysql:8.0'],
+                'elasticsearch' => ['ajardin/magento2-elasticsearch:7'],
+                'php' => ['ajardin/php:8.1'],
+                'redis' => ['redis:6-alpine'],
             ],
-            '2.3' => [
-                'database' => ['mariadb:10.3', 'mariadb:10.2', 'mariadb:10.1', 'mysql:5.7', 'mysql:5.6'],
-                'elasticsearch' => ['ajardin/magento2-elasticsearch:7', 'ajardin/magento2-elasticsearch:6'],
+            '2.4.4' => [
+                'database' => ['mariadb:10.4', 'mysql:8.0'],
+                'elasticsearch' => ['ajardin/magento2-elasticsearch:7'],
+                'php' => ['ajardin/php:8.1'],
+                'redis' => ['redis:6-alpine'],
+            ],
+            '2.4.3' => [
+                'database' => ['mariadb:10.4', 'mysql:8.0'],
+                'elasticsearch' => ['ajardin/magento2-elasticsearch:7'],
+                'php' => ['ajardin/php:7.4'],
+                'redis' => ['redis:6-alpine'],
+            ],
+            '2.4.2' => [
+                'database' => ['mariadb:10.4', 'mysql:8.0'],
+                'elasticsearch' => ['ajardin/magento2-elasticsearch:7'],
+                'php' => ['ajardin/php:7.4'],
+                'redis' => ['redis:6-alpine'],
+            ],
+            '2.4.1' => [
+                'database' => ['mariadb:10.4', 'mysql:8.0'],
+                'elasticsearch' => ['ajardin/magento2-elasticsearch:7'],
+                'php' => ['ajardin/php:7.4'],
+                'redis' => ['redis:5-alpine'],
+            ],
+            '2.4.0' => [
+                'database' => ['mariadb:10.4', 'mariadb:10.3', 'mariadb:10.2', 'mysql:8.0', 'mysql:5.7'],
+                'elasticsearch' => ['ajardin/magento2-elasticsearch:7'],
                 'php' => ['ajardin/php:7.4', 'ajardin/php:7.3'],
-                'redis' => ['redis:6-alpine', 'redis:5-alpine'],
+                'redis' => ['redis:5-alpine'],
             ],
         ],
 
         // https://doc.oroinc.com/backend/setup/system-requirements/
         EnvironmentEntity::TYPE_OROCOMMERCE => [
             '5.0' => [
-                'database' => ['mysql:8.0', 'postgres:13-alpine', 'postgres:12-alpine'],
+                'database' => ['mysql:8.0', 'postgres:13-alpine'],
                 'elasticsearch' => ['elasticsearch:7.12.0'],
-                'php' => ['ajardin/php:8.0', 'ajardin/php:7.4'],
-                'rabbitmq' => ['ajardin/orocommerce-rabbitmq:3.8'],
-                'redis' => ['redis:6-alpine', 'redis:5-alpine'],
+                'php' => ['ajardin/php:8.0'],
+                'rabbitmq' => ['ajardin/orocommerce-rabbitmq:3.9'],
+                'redis' => ['redis:6-alpine'],
             ],
             '4.2' => [
                 'database' => ['mysql:8.0', 'postgres:13-alpine', 'postgres:12-alpine'],
@@ -97,9 +121,13 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
         // https://docs.sylius.com/en/latest/book/installation/requirements.html
         EnvironmentEntity::TYPE_SYLIUS => [
+            '1.11' => [
+                'database' => ['mysql:8.0', 'mysql:5.7'],
+                'php' => ['ajardin/php:8.1', 'ajardin/php:8.0'],
+            ],
             '1.10' => [
                 'database' => ['mysql:8.0', 'mysql:5.7'],
-                'php' => ['ajardin/php:8.0', 'ajardin/php:7.4', 'ajardin/php:7.3'],
+                'php' => ['ajardin/php:8.1', 'ajardin/php:8.0', 'ajardin/php:7.4'],
             ],
             '1.9' => [
                 'database' => ['mysql:8.0', 'mysql:5.7'],
@@ -113,21 +141,17 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
         // https://symfony.com/doc/current/setup.html#technical-requirements
         EnvironmentEntity::TYPE_SYMFONY => [
-            '5.3' => [
-                'database' => ['mariadb:10.5', 'mariadb:10.4', 'mariadb:10.3', 'mariadb:10.2', 'mariadb:10.1', 'mysql:8.0', 'mysql:5.7', 'mysql:5.6', 'postgres:13-alpine', 'postgres:12-alpine', 'postgres:11-alpine', 'postgres:10-alpine', 'postgres:9-alpine'],
-                'php' => ['ajardin/php:8.0', 'ajardin/php:7.4', 'ajardin/php:7.3'],
+            '6.0' => [
+                'database' => ['mariadb:10.7', 'mariadb:10.6', 'mariadb:10.5', 'mariadb:10.4', 'mariadb:10.3', 'mariadb:10.2', 'mariadb:10.1', 'mysql:8.0', 'mysql:5.7', 'mysql:5.6', 'postgres:14-alpine', 'postgres:13-alpine', 'postgres:12-alpine', 'postgres:11-alpine', 'postgres:10-alpine', 'postgres:9-alpine'],
+                'php' => ['ajardin/php:8.1', 'ajardin/php:8.0'],
             ],
-            '5.2' => [
-                'database' => ['mariadb:10.5', 'mariadb:10.4', 'mariadb:10.3', 'mariadb:10.2', 'mariadb:10.1', 'mysql:8.0', 'mysql:5.7', 'mysql:5.6', 'postgres:13-alpine', 'postgres:12-alpine', 'postgres:11-alpine', 'postgres:10-alpine', 'postgres:9-alpine'],
-                'php' => ['ajardin/php:8.0', 'ajardin/php:7.4', 'ajardin/php:7.3'],
+            '5.4' => [
+                'database' => ['mariadb:10.7', 'mariadb:10.6', 'mariadb:10.5', 'mariadb:10.4', 'mariadb:10.3', 'mariadb:10.2', 'mariadb:10.1', 'mysql:8.0', 'mysql:5.7', 'mysql:5.6', 'postgres:14-alpine', 'postgres:13-alpine', 'postgres:12-alpine', 'postgres:11-alpine', 'postgres:10-alpine', 'postgres:9-alpine'],
+                'php' => ['ajardin/php:8.1', 'ajardin/php:8.0', 'ajardin/php:7.4', 'ajardin/php:7.3'],
             ],
             '4.4' => [
-                'database' => ['mariadb:10.5', 'mariadb:10.4', 'mariadb:10.3', 'mariadb:10.2', 'mariadb:10.1', 'mysql:8.0', 'mysql:5.7', 'mysql:5.6', 'postgres:13-alpine', 'postgres:12-alpine', 'postgres:11-alpine', 'postgres:10-alpine', 'postgres:9-alpine'],
-                'php' => ['ajardin/php:8.0', 'ajardin/php:7.4', 'ajardin/php:7.3'],
-            ],
-            '3.4' => [
-                'database' => ['mariadb:10.5', 'mariadb:10.4', 'mariadb:10.3', 'mariadb:10.2', 'mariadb:10.1', 'mysql:8.0', 'mysql:5.7', 'mysql:5.6', 'postgres:13-alpine', 'postgres:12-alpine', 'postgres:11-alpine', 'postgres:10-alpine', 'postgres:9-alpine'],
-                'php' => ['ajardin/php:8.0', 'ajardin/php:7.4', 'ajardin/php:7.3'],
+                'database' => ['mariadb:10.7', 'mariadb:10.6', 'mariadb:10.5', 'mariadb:10.4', 'mariadb:10.3', 'mariadb:10.2', 'mariadb:10.1', 'mysql:8.0', 'mysql:5.7', 'mysql:5.6', 'postgres:14-alpine', 'postgres:13-alpine', 'postgres:12-alpine', 'postgres:11-alpine', 'postgres:10-alpine', 'postgres:9-alpine'],
+                'php' => ['ajardin/php:8.1', 'ajardin/php:8.0', 'ajardin/php:7.4', 'ajardin/php:7.3'],
             ],
         ],
     ]);
