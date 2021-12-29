f501b1f38b85f5e11b8d4a03a795b98a4eb24eb8
Alexandre Jardin <info@ajardin.fr>
2021-12-29 10:06:03 +0100

    Allow to specify a custom path to the database backup file

--- src/Command/Database/DumpCommand.php
+++ src/Command/Database/DumpCommand.php
@@ -10,6 +10,7 @@ use App\Service\ApplicationContext;
 use App\Service\Middleware\Database;
 use App\Service\Wrapper\OrigamiStyle;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -31,6 +32,19 @@ class DumpCommand extends AbstractBaseCommand
     /**
      * {@inheritdoc}
      */
+    protected function configure(): void
+    {
+        $this->addArgument(
+            'path',
+            InputArgument::OPTIONAL,
+            'The relative path to the dump file in the project directory',
+            Database::DEFAULT_BACKUP_FILENAME
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new OrigamiStyle($input, $output);
@@ -43,7 +57,9 @@ class DumpCommand extends AbstractBaseCommand
                 $this->printEnvironmentDetails($environment, $io);
             }
 
-            $this->database->dump($environment->getLocation().'/'.Database::DEFAULT_BACKUP_FILENAME);
+            $path = ltrim($input->getArgument('path'), '/');
+            $this->database->dump($environment->getLocation().'/'.$path);
+
             $io->success('Database dump successfully executed.');
         } catch (OrigamiExceptionInterface $exception) {
             $io->error($exception->getMessage());
--- src/Command/Database/RestoreCommand.php
+++ src/Command/Database/RestoreCommand.php
@@ -10,6 +10,7 @@ use App\Service\ApplicationContext;
 use App\Service\Middleware\Database;
 use App\Service\Wrapper\OrigamiStyle;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -31,6 +32,19 @@ class RestoreCommand extends AbstractBaseCommand
     /**
      * {@inheritdoc}
      */
+    protected function configure(): void
+    {
+        $this->addArgument(
+            'path',
+            InputArgument::OPTIONAL,
+            'The relative path to the dump file in the project directory',
+            Database::DEFAULT_BACKUP_FILENAME
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new OrigamiStyle($input, $output);
@@ -43,7 +57,9 @@ class RestoreCommand extends AbstractBaseCommand
                 $this->printEnvironmentDetails($environment, $io);
             }
 
-            $this->database->restore($environment->getLocation().'/'.Database::DEFAULT_BACKUP_FILENAME);
+            $path = ltrim($input->getArgument('path'), '/');
+            $this->database->restore($environment->getLocation().'/'.$path);
+
             $io->success('Database restore successfully executes.');
         } catch (OrigamiExceptionInterface $exception) {
             $io->error($exception->getMessage());
